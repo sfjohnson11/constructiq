68549c729d90bc82cc1016608ee90bdded2c440a
sfjohnson11 <info@sfjohnsonconsulting.com>
2025-06-24 13:12:42 -0700

    Create next,config.js

---
+++ next,config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+  // You can add more Next.js config options here if needed in the future.
+};
+
+module.exports = nextConfig;
