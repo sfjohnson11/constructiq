a4af47fe8a3a0fc1f98e31921e335aa8c8a8fb71
sfjohnson11 <info@sfjohnsonconsulting.com>
2025-06-26 10:20:26 -0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,11 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
+const config = {
   content: [
     "./app/**/*.{ts,tsx,js,jsx}",
     "./components/**/*.{ts,tsx,js,jsx}"
   ],
   theme: {
     extend: {
-      // You can customize the color palette here. For example:
-      // colors: {
-      //   'brand-blue-dark': '#1e3a8a',
-      //   'brand-blue-light': '#3b82f6',
-      //   'brand-yellow': '#facc15'
-      // }
+      // You can customize your theme here
     }
   },
   plugins: []
