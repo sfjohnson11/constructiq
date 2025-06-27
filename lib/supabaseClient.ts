24c33102e210ba1e7c7c680abe5aeadf4c29de09
sfjohnson11 <info@sfjohnsonconsulting.com>
2025-06-27 08:37:45 -0700

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,4 +1,3 @@
-// lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
@@ -11,5 +10,6 @@ if (!supabaseUrl || !supabaseAnonKey) {
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export default supabase
+
 
 
